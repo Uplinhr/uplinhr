7a9a04288d96008c61abef6e946eb31f1b7a1e94
Tomas <tomiiulman@gmail.com>
2025-07-23 16:27:17 -0300

    Una interfaz nueva para evitar un error

--- src/app/planes/page.tsx
+++ src/app/planes/page.tsx
@@ -1,10 +1,12 @@
 import CardsPlan from "@/views/cardsPlan";
+import CardsPlanLanding from "@/views/cardsPlanLanding";
 import {QAView} from "@/views/qaGeneral";
 import {QAViewP} from "@/views/qaPayments";
 const Planes = () => {
   return (
     <section className="bg-white h-auto">
    <CardsPlan/>   
+   <CardsPlanLanding/>
     <QAView/>
     <QAViewP/>
     </section>
