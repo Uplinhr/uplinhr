c82ea1c4bc0944703c15465f8cd9d92dffce144f
elpista <marcopistagnesi2003@gmail.com>
2025-07-23 19:31:20 -0300

    Avanzando con la landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 import Button from "@/components/Button/Button";
+import Solutions from "@/views/Solutions";
+import Presentation from "@/views/Presentation";
+import CardsPlanLanding from "@/views/cardsPlanLanding";
 
 export default function TestTailwind() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-cyan-500 to-blue-700 ">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4 bg-red">
-        Bienvenido Marco a la Landing de bienvenida
-      </h1>
-        <Button
-        link='/'
-        tag= 'Click aca'
-        mode= {0}
-        height={60}
-        width={200}>
-        </Button>
+      <Presentation/>
+      <Solutions/>
+      <CardsPlanLanding/>
     </div>
   );
 }
---
+++ src/views/Solutions.tsx
@@ -0,0 +1,29 @@
+import CardSolution from "@/components/CardSolution/CardSolution"
+
+const Solutions = () => {
+    return(
+        <div>
+            <h1 className="font-poppins text-[28px] text-black font-semibold text-center mb-8">Soluciones de talento flexibles, con el acompañamiento que necesitás</h1>
+            <div className="flex flex-wrap gap-6 justify-center">
+                <CardSolution
+                icon= {"/iconFlexibilidad.svg"}
+                title = {'Flexibilidad'}
+                description = "Adaptabilidad a las necesidades cambiantes de la empresa."/>
+                <CardSolution
+                icon= {"/iconAhorroCostos.svg"}
+                title = {'Ahorro de costos'}
+                description = "Más valor, menos costos asociados a consultorías tradicionales."/>
+                <CardSolution
+                icon= {"/iconAccesoExpertos.svg"}
+                title = {'Acceso a expertos'}
+                description = "Disponibilidad de un equipo de RRHH especializado."/>
+                <CardSolution
+                icon= {"/iconEscalabilidad.svg"}
+                title = {'Escalabilidad'}
+                description = "Ajuste de servicios según el crecimiento o la necesidad."/>
+            </div>
+        </div>
+    )
+}
+
+export default Solutions
