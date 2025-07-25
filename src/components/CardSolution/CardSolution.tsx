8727c78b2c16339a4b80fdc557b899e6332235af
AndreaVPalacios <andreita.estudia@gmail.com>
2025-07-25 19:13:19 -0400

    Pequeños detallitos
    Perfeccionando la landing page

--- src/components/CardSolution/CardSolution.tsx
+++ src/components/CardSolution/CardSolution.tsx
@@ -1,36 +1,26 @@
-import {SolutionProps} from "@/interfaces";
+import { SolutionProps } from "@/interfaces";
 import Image from "next/image";
 
-const CardSolution = ({
-    icon,
-    title,
-    description
-}: SolutionProps) => {
-
-    return(
-        <div className="transform transition-transform duration-200 hover:scale-[1.05] bg-white rounded-lg shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]
+const CardSolution = ({ icon, title, description }: SolutionProps) => {
+  return (
+    <div
+      className="transform transition-transform duration-200 hover:scale-[1.05] bg-white rounded-lg shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]
         flex flex-col items-center text-center pt-6"
-        style={{
-            width: '285px',
-            height: '200px',
-            margin: '5px'
-        }}>
-            <div className="mb-4">
-                <Image
-                src={icon}
-                alt="Logo Uplin"
-                width={57}
-                height={52}
-                />
-            </div>
-            <h5 className="font-poppins font-bold text-[18px]">
-                {title}
-            </h5>
-            <p className="font-poppins font-normal text-[16px] text-[#737373] px-4">
-                {description}
-            </p>
-        </div>
-    )
-}
+      style={{
+        width: "285px",
+        height: "200px",
+        margin: "5px",
+      }}
+    >
+      <div className="mb-4">
+        <Image src={icon} alt="Logo Uplin" width={57} height={52} />
+      </div>
+      <h5 className="font-poppins font-bold text-[16px]">{title}</h5>
+      <p className="font-poppins font-normal text-[14px] text-[#737373] px-4">
+        {description}
+      </p>
+    </div>
+  );
+};
 
-export default CardSolution
+export default CardSolution;
--- src/views/cardsPlanLanding.tsx
+++ src/views/cardsPlanLanding.tsx
@@ -1,19 +1,21 @@
-import PlanCard from '@/components/planCard/PlanCard';
-import { PLANS_DATA } from '@/utils/plans';
-import Image from 'next/image';
-import Link from 'next/link';
+import PlanCard from "@/components/planCard/PlanCard";
+import { PLANS_DATA } from "@/utils/plans";
+import Image from "next/image";
+import Link from "next/link";
 
 const CardsPlanLanding = () => {
   const nonCustomPlans = PLANS_DATA.filter((plan) => !plan.isCustom);
 
   return (
     <div>
-      <div className='text-center font-poppins mb-2 px-4 md:px-0'>
-        <h1 className='text-[28px] font-semibold'>Crecé tu equipo con Uplin</h1>
-        <p className='text-[16px] max-w-200 mx-auto'>
-          Todos nuestros planes incluyen acompañamiento estratégico, 
-          recursos listos para usar, acceso a plataforma y un sistema 
-          de créditos para búsquedas de talento.
+      <div className="text-center font-poppins mb-2 px-4 md:px-0">
+        <h1 className="text-[28px] font-semibold mb-3">
+          Crecé tu equipo con Uplin
+        </h1>
+        <p className="text-[16px] max-w-200 mx-auto mb-5">
+          Todos nuestros planes incluyen acompañamiento estratégico, recursos
+          listos para usar, acceso a plataforma y un sistema de créditos para
+          búsquedas de talento.
         </p>
       </div>
       <section className="py-12 px-4 max-w-7xl mx-auto">
@@ -28,36 +30,38 @@ const CardsPlanLanding = () => {
         </div>
       </section>
 
-      <div className='bg-[#6C4099] mx-4 md:mx-14 py-16 rounded-[25px] mb-16'>
-        <div className='flex flex-col md:flex-row flex-wrap gap-6 justify-center items-center font-poppins'>
-
-          <Image 
-            className='md:mr-12 mb-6 md:mb-0 rounded-lg'
-            src={'/imgPlanCustom.png'} 
-            alt={'Imagen plan custom'}
+      <div className="bg-[#6C4099] mx-4 md:mx-14 py-16 rounded-[25px] mb-16">
+        <div className="flex flex-col md:flex-row flex-wrap gap-6 justify-center items-center font-poppins">
+          <Image
+            className="md:mr-12 mb-6 md:mb-0 rounded-lg"
+            src={"/imgPlanCustom.png"}
+            alt={"Imagen plan custom"}
             width={512}
             height={256}
             priority
-            style={{ maxWidth: '100%', height: 'auto' }}
+            style={{ maxWidth: "100%", height: "auto" }}
           />
 
-          <div className='text-start text-white max-w-lg px-4 md:px-0'>
-            <h4 className='text-[20px] font-medium mb-2'>
+          <div className="text-start text-white max-w-lg px-4 md:px-0">
+            <h4 className="text-[20px] font-medium mb-2">
               ¿Ninguno de los planes se ajusta a lo que necesitás?
             </h4>
-            <p className='text-[16px] max-w-106 mb-2'>
-              Diseñamos una propuesta a medida para acompañarte en ese desafío puntual.
+            <p className="text-[16px] max-w-106 mb-2">
+              Diseñamos una propuesta a medida para acompañarte en ese desafío
+              puntual.
             </p>
-            <p className='font-light text-[15px]'>
+            <p className="font-light text-[15px]">
               <span className="text-white mr-2">✓</span> Diagnóstico sin costo
-              <br/>
-              <span className="text-white mr-2">✓</span> Soluciones personalizadas según el contexto
-              <br/>
-              <span className="text-white mr-2">✓</span> Entregables claros, tiempos definidos 
-              <br/>
+              <br />
+              <span className="text-white mr-2">✓</span> Soluciones
+              personalizadas según el contexto
+              <br />
+              <span className="text-white mr-2">✓</span> Entregables claros,
+              tiempos definidos
+              <br />
               <span className="text-white mr-2">✓</span> Acompañamiento experto
-              <br/>
-              <Link href={'https://www.uplinhr.com/fgsdf3254h'}>
+              <br />
+              <Link href={"https://www.uplinhr.com/fgsdf3254h"}>
                 <button
                   className={`min-w-50 min-h-4 mt-4 hover:cursor-pointer rounded-[6px] border border-white 
                     text-center font-[600] text-[16px] leading-[48px] transform transition-transform 
