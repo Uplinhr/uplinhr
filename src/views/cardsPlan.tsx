c5eda07e546f930190e36e38e5c6840958a65cbb
Tomas <tomiiulman@gmail.com>
2025-07-29 19:17:42 -0300

    Nuevos cambios

--- src/views/cardsPlan.tsx
+++ src/views/cardsPlan.tsx
@@ -4,7 +4,7 @@ import { PLANS_DATA } from "@/utils/plans";
 const CardsPlanView = () => {
   return (
     <section className="py-12 px-4 max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 md:gap-16 lg:gap-8">
         {PLANS_DATA.map((plan) => (
           <PlanCard
             key={plan.plan}
