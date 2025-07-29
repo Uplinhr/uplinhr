083e2ad5530e96a85740020fcd7ba31fe94d391b
Tomas <tomiiulman@gmail.com>
2025-07-29 16:03:43 -0300

    Cambio en navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -72,6 +72,14 @@ const Navbar = () => {
             >
               Planes
             </a>
+            
+            <a
+              className="block text-[#502B7D] hover:text-[#6b4699] py-2 px-4 hover:bg-[#502B7D]/10 rounded transition-colors cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            >
+              Uplin Academy
+            </a>
+
             <a
               className="inline-block border-[#502B7D] border-2 px-5 py-1 rounded-lg hover:bg-[#502B7D] hover:text-white transition-colors mt-2 cursor-pointer"
               onClick={() => setIsOpen(false)}
