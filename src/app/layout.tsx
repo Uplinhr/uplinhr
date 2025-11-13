de68f34225c4d2e080423411bc2b949a40ccbbe9
dani04-maker <dacevedob04@gmail.com>
2025-11-12 18:02:12 -0800

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,16 @@ export const metadata: Metadata = {
   title: "Uplin",
   description: "RRHH Flexible para tu Empresa",
   icons: {
-    icon: "/logoUplin.svg",
-    apple: "/logoUplin.svg",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon-180x180.png", sizes: "180x180", type: "image/png" }],
+    other: [{ rel: "android-chrome", url: "/favicon-192x192.png", sizes: "192x192" }],
   },
 };
+
 
 export default function RootLayout({
   children,
